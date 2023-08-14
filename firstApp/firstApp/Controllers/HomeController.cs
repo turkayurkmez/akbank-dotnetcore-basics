4df91b3e0fce7f2c40278c9e30c12a1d09de22c9
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-14 14:41:56 +0300

    First app developing...

---
+++ firstApp/firstApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using firstApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace firstApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.UserName = "Türkay";
+            ViewBag.Hour = DateTime.Now.Hour;
+
+            //dynamic value = 9;
+            //value.
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ firstApp/firstApp/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace firstApp.Models
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+    }
+}
