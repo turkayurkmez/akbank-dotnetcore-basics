a602c72d8cf62a90aba4056b86abff6403dcd9c1
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-15 14:33:57 +0300

    Creating and rendering view component

---
+++ eshop/eshop/Components/MenuViewCompoment.cs
@@ -0,0 +1,23 @@
+﻿using eshop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+
+        private readonly ICategoryService _categoryService;
+
+        public MenuViewComponent(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            //kategorileri çek ve menüyü (görsel) oluştur.
+            var categories = _categoryService.GetCategories();
+            return View(categories);
+        }
+    }
+}
