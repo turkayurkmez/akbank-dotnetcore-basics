cba895e2456846a0cf70a6f470d96fa4c01c8fb3
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-16 12:29:35 +0300

    Products Controller for Create and Edit product

---
+++ eshop/eshop/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using eshop.Models;
+using eshop.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductsController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public IActionResult Index()
+        {
+            var products = productService.GetProducts();
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            //Product nesnesi, kurallara uygun mu?
+            //Uygunsa ekle
+            //Değilse hata bildir. 
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var product = productService.FindProduct(id);
+            return View(product);
+        }
+    }
+}
