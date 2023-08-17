72ed2cccf157d84f11463e908cfe59c37124ede5
Türkay Ürkmez <turkay@turkayurkmez.com>
2023-08-17 14:43:05 +0300

    talking about API

---
+++ eshop/eshop.API/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eshop.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = new string[] { "P1", "P2", "P3" };
+            return Ok(products);
+        }
+    }
+}
